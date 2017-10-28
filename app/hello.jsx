bb651f7e2ed9ffcaad67c6a26b23a2760b43dcca
wendy-92 <960426728@qq.com>
2017-10-28 23:58:39 +0800

    add hello components

---
+++ app/hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class hello extends React.PureComponent{
+    render(){
+        return(
+            <h1>hello, world</h1>
+        )
+    }
+}
+
+export default hello;
--- app/index.js
+++ app/index.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import hello from './hello';
+
 ReactDOM.render((
   <div>
-    {1}
+    1
+    {hello}
   </div>
 ), document.getElementById('app'));
