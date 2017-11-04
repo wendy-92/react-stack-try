f7a4a6bfd353b0ffe1519bfe3f8a2ce7c217b4ce
wendy-92 <960426728@qq.com>
2017-11-04 23:55:20 +0800

    create app no react-router

--- router/index.js
+++ router/index.js
@@ -4,13 +4,37 @@ import { Router, Route, hashHistory } from 'react-router';
 import Hello from '../app/Hello';
 
 class ProviderRouter extends React.Component{
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            router: window.location.hash.substr(1),
+        }
+    }
+    componentDidMount(){
+        window.addEventListener('hashChange', () => {this.setState({
+            route: window.location.hash.substr(1),
+        })});
+    }
+
+    render(){
+        const child = '';
+        switch(this.state.router){
+            case 'hello': 
+              return <Hello />;
+            default: 
+              return <div />;
+        }
+        console.log(this.state.router);
         return(
-            <Router history={hashHistory}>
-                <Route path="/" component={Hello}/>
-            </Router>
-        )
+        <div>
+            <h1>App</h1>
+            <ul>
+            <li><a href="#/about">About</a></li>
+            <li><a href="#/hello">Inbox</a></li>
+            </ul>
+            {child}
+        </div>
+        );
     }
 }
-
 export default ProviderRouter;
